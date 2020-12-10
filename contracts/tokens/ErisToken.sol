7b0a9fcbb6767f2330ebdb077e094f297e489f83
cyotee doge <cyotee@gmail.com>
2020-12-09 19:26:41 -0800

    Updated to Solidity 0.7.4

---
+++ contracts/tokens/ErisToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+import "hardhat/console.sol";
+
+import "../abstract/Divine.sol";
+import "../dependencies/holyzeppelin/contracts/math/SafeMath.sol";
+
+contract ErisToken is Divine {
+
+    using SafeMath for uint256;
+
+    uint8 public transferFeePercentageX100;
+    
+    constructor () Divine( "ERIS", "ERIS" ) {
+        console.log("ERIS::constructor: Instantiating ERIS");
+        console.log("ERIS::constructor: Instantiated ERIS");
+    }
+}
